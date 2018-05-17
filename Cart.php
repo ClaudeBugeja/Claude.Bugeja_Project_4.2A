640561932d86292d08e532f6466c90c0dac0647d
Claude Bugeja <b42219@mcast.org.mt>
2018-05-17 13:27:39 +0200

    --Created Cart to display products in cart table --Data page is done(Log In and Registration)

---
+++ Cart.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+?>
+<html>
+    <head>
+        <script src="https://www.paypalobjects.com/api/checkout.js"></script>
+        <title>Redefined</title>
+        <link rel="icon" href="images/blacklogo.png" type="image" width="20" height="20">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="Home.css">
+    </head>
+    <body>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                 
+          
+          <button  class="navbar-toggler"  type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" ></span>
+          </button >
+                
+                <a class="navbar-brand" href="index.php">
+                <img id="logo" src="images/logo-white.png" alt="">
+              </a> 
+                
+                <a href="Data.php">
+                    <img  id="profile"  src="images/profile.png" width="20" height="20">
+                </a>
+                
+                  <a href="Search.php">
+               <img id="searchmob" src="images/searching.png" style="width: 20px; height: 20px; ">
+              </a>  
+                
+                <a href="Cart.php" >
+                   <img id="bag" src="images/bag-logo.png" width="20" height="20">
+                </a>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+              
+           <ul class="navbar-nav" style="width: 100%;">
+                
+                  <li class="nav-item active">
+                    <a class="nav-link" href="Mobile.php">Mobile<span class="sr-only">(current)</span></a>
+                  </li>
+                    <hr/>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="Mobile.php">Computing</a>
+                     
+                  </li>
+               <hr/>
+                  <li class="nav-item active">
+                    <a class="nav-link disabled" href="Mobile.php">Peripherals</a>
+                     
+                  </li>
+                <hr/>
+                    <li class="nav-item active">
+                    <a class="nav-link disabled" href="Mobile.php">Vision</a>
+                  
+                  </li>
+              <hr/>
+                    <li class="nav-item active">
+                    <a class="nav-link disabled" href="Mobile.php">Sound</a>
+
+                  </li>
+              <hr/>
+                    <li class="nav-item active">
+                    <a class="nav-link disabled" href="Mobile.php">Gaming</a>
+                  </li>
+               <hr/>
+
+             
+                </ul>
+              <a href="Data.php" style="margin-left:10%; margin-right: 5%;">
+               <img id="profiledesk" src="images/profile.png" href="inde1.html" style="width: 20px; height: 20px; ">
+              </a>
+              
+             
+             <a href="Search.php" style="margin-right: 5%;">
+               <img id="search" src="images/searching.png" style="width: 20px; height: 20px; ">
+              </a>     
+                  
+              <a href="Cart.php" style="margin-right: 5%;">
+               <img id="bagdesk" src="images/bag-logo.png" style="width: 20px; height: 20px;">
+              </a> 
+              
+              <form action="index.php" method="post">
+              <button type="submit" style="margin-right: 5%; border:none;" name="logout">
+               <img src="images/logout.png" href="inde1.html" style="margin-bottom:20%; width: 20px; height: 20px; ">
+              </button>
+              </form>
+              <?php
+                if(isset($_POST['logout'])){
+                    session_destroy();
+                }
+              ?>
+          </div>
+                             
+               
+            </div>
+        </nav>
+    <br/>
+<br/>
+        <h3 style="margin-left:2%;"><?php echo "$_SESSION[name]" ?>'s Cart</h3>
+        <br/>
+        <br/>
+        <div class="col-lg-12" style="width:100%; padding-left:2%; padding-right:2%;">
+			<div class="table-responsive" >
+				<table class="table table-bordered" >
+					<tr>
+						<th width="50%">Item Name</th>
+						<th width="50%">Price</th>
+					</tr>
+					<tr>
+                        <?php
+                            $conn = mysqli_connect('localhost','root','','project') or die("hello");
+                            $query = "Select ProductID from cart where AccountID = '$_SESSION[AccountID]'";
+                            $result = mysqli_query($conn, $query);
+                            while($row = mysqli_fetch_assoc($result)) {
+                                $query1 = "Select ProductName, Price, Image from product where ProductID = '$row[ProductID]'";
+                                $result1 = mysqli_query($conn, $query1);
+                                 while($row = mysqli_fetch_assoc($result1)){
+                                    
+                                    ?>
+                                      <td><?php echo "$row[ProductName]"; ?></td>
+						              <td><?php echo "$row[Price]"; ?></td>
+                                        </tr>
+                                    <?php
+                                }
+                            
+                            }
+                                
+                                
+                        ?>
+						
+					
+                </table>
+			</div>
+        </div>
+        <br/>
+        <br/>
+        <div id="paypal-button" style="padding-left:2%;"></div>
+        <div id="paypal-success" style="padding-left:2%; color:green;"></div>
+        <div id="paypal-es" style="padding-left:2%; color:green;"></div>
+
+  <script>
+
+      paypal.Button.render({
+
+        env: 'sandbox', // Or 'sandbox'
+          
+          style: {
+        color: 'black',
+        size: 'small'
+      },
+
+        client: {
+            sandbox:    'AW8bRhCm6WrpOnjPN1wfKNSXBcZZJTkIJtZUOfPlBV0EIMHYT5cVU8HAyctOBwANOwBQ1pugF-07lsBF',
+            production: 'AW8bRhCm6WrpOnjPN1wfKNSXBcZZJTkIJtZUOfPlBV0EIMHYT5cVU8HAyctOBwANOwBQ1pugF-07lsBF'
+        },
+
+        commit: true, // Show a 'Pay Now' button
+
+        payment: function(data, actions) {
+            return actions.payment.create({
+                payment: {
+                    transactions: [
+                        {
+                            amount: { total: '1.00', currency: 'USD' }
+                        }
+                    ]
+                }
+            });
+        },
+
+        onAuthorize: function(data, actions) {
+            return actions.payment.execute().then(function(payment) {
+               
+                document.getElementById('paypal-success').innerHTML="Payment Successfull";
+                
+                window.location.replace("paypalConfirm.php");
+                
+            });
+        }
+
+    }, '#paypal-button');
+  </script>
+      <!--Footer-->
+            <footer class="page-footer font-small stylish-color-dark pt-4 mt-4" style="background-color:#E8E8E8">
+      
+          <!--Footer Links-->
+          <div class="container text-center text-md-left">
+      
+              <!-- Footer links -->
+              <div class="row text-center text-md-left mt-3 pb-3">
+      
+                  <!--First column-->
+                  <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                      <h6 class="text-uppercase mb-4 font-weight-bold">REDEFINED</h6>
+                      <p>Redefining Shopping in every way. Cheapest prices , the best client support and the most user-friendly website out there to encourage every demographic to get onboard in redifine shopping.</p>
+                  </div>
+                  <!--/.First column-->
+      
+                  <hr class="w-100 clearfix d-md-none">
+      
+                  <!--Second column-->
+                  <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                      <h6 class="text-uppercase mb-4 font-weight-bold">Information</h6>
+                      <p><a href="OpenHours.php" style="color: black;">Opening Hours</a></p>
+                      <p><a href="AboutUs.php" style="color: black;">About Us</a></p>
+                      <p><a href="Contact.php" style="color: black;">Contact Us</a></p>
+                    
+                  </div>
+                  <!--/.Second column-->
+      
+                  <hr class="w-100 clearfix d-md-none">
+      
+                  
+      
+                  
+      
+                  <!--Fourth column-->
+                  <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                      <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                      <p><img src="images/homeLogo.png" style="width: 7%;"> Malta, RBT 1121, Rabat</p>
+                      <p><img src="images/envelopeLogo.png" style="width: 7%;"> Claude.Bugeja09@Gmail.com</p>
+                      <p><img src="images/phoneLogo.png" style="width: 7%;"> + 356 234 567 88</p>
+                      <p><img src="images/homePhoneLogo.png" style="width: 7%;"> + 21 234 567 89</p>
+                  </div>
+                  <!--/.Fourth column-->
+      
+              </div>
+              <!-- Footer links -->
+      
+              <hr>
+      
+              <div class="row py-3 d-flex align-items-center">
+      
+                  <!--Grid column-->
+                  <div class="col-md-8 col-lg-7">
+      
+                      <!--Copyright-->
+                      <p class="text-center text-md-left grey-text">© 2018 Copyright: <a href="inde1.html" style="color: black;"><strong> Redefined.com</strong></a></p>
+                      <!--/.Copyright-->
+      
+                  </div>
+                  <!--Grid column-->
+      
+                  <!--Grid column-->
+                  
+      
+              </div>
+      
+          </div>
+      
+      </footer>
+      <!--/.Footer-->
+                      
+  
+        <script>
+            document.getElementById('searchmob').style.display = "none";
+            document.getElementById('bagdesk').style.display = "none";
+            document.getElementById('profiledesk').style.display = "none";
+            function myFunction(x) {
+                if (x.matches) { // If media query matches
+                    document.getElementById('profile').style.display = "block";
+                    document.getElementById('bag').style.display = "block";
+                    document.getElementById('searchmob').style.display = "block";
+                    document.getElementById('bagdesk').style.display = "none";
+                    document.getElementById('profiledesk').style.display = "none";
+                    document.getElementById('search').style.display = "none";
+                } else {
+                    document.getElementById('searchmob').style.display = "none";
+                    document.getElementById('search').style.display = "block";
+                    document.getElementById('profile').style.display = "none";
+                    document.getElementById('bag').style.display = "none";
+                    document.getElementById('bagdesk').style.display = "block";
+                    document.getElementById('profiledesk').style.display = "block";
+                }
+            }
+            var x = window.matchMedia("(max-width: 770px)")
+            myFunction(x) // Call listener function at run time
+            x.addListener(myFunction) // Attach listener function on state changes     
+        </script>      
+
+    </body>
+</html>
---
+++ Data.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+?>
+<html>
+    
+    <head>
+        <title>Redefined</title>
+        <link rel="icon" href="images/blacklogo.png" type="image" width="20" height="20">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="Login.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    
+    <body style="background-color:#F5F5F5; width:100%;">
+        <br/>
+        
+        <div class="row nopadding" style="width:100%;">
+            <div class="col-lg-2 col-sm-2 nopadding"></div>
+        <div class="col-lg-8 col-sm-12 nopadding" style="text-align: center;">
+            <a href="index.php">
+                <img id="logo" src="images/Black_Logo.png">
+            </a>
+        </div>
+            <div class="col-lg-2 col-sm-2 nopadding"></div>
+        </div>
+        <br/>
+        <div class="row nopadding" style="width:100%;">
+            <div class="col-lg-4 col-sm-4 nopadding"></div>
+        <div class="col-lg-2 nopadding" style="background-color:white; height:100%;">
+          
+                <div onclick="myFunction()" class="col-lg-12">
+                    <h5 style="text-align: center; margin:5%;">NEW TO REDIFINED?</h5>
+                </div>
+     
+        </div>
+        
+        <div class="col-lg-2 nopadding" style="background-color:white; height:100%; border-left: 2px solid black;">
+                <div onclick="myFunction()" class="col-lg-12">
+                    <h5 style="text-align: center; margin:5%;">ALREADY REGISTERED?</h5>
+                </div>
+
+        </div>
+            <div class="col-lg-4 nopadding"></div>
+        </div>
+        <br/> 
+        <div class="row nopadding" style="width:100%;">
+            <div class="col-lg-4 nopadding"></div>
+        <div class="col-lg-4 nopadding" style="background-color:white; height:100%;">
+            <br/>
+            
+            <div  id="LogIn">
+                <h5   style="font-weight: bold; text-align: center;">SIGN IN WITH EMAIL</h5>
+                <form method="POST" action="Data.php">
+                  <div class="form-group" style="margin-left:5%; margin-right:5%;">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" name="email" placeholder="Enter email">
+                  
+                
+                  </div>
+                  <div class="form-group" style="margin-left:5%; margin-right:5%;">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" name="Password" placeholder="Password">
+                    <a href="FPass.php" style="color:black;">
+                        <small id="emailHelp" class="form-text text-muted">Forgot Password?</small></a>
+                    </div>
+                    <?php
+                    if(isset($_POST['Submit'])){
+                        $Username = $_POST["email"];
+                        $Password = $_POST["Password"];
+                        $conn = mysqli_connect('localhost', 'root', '', 'project') 
+                                or die('Cannot connect to db');
+                        $query = "select AccountID, Email, Password, FirstName from user where Email = '$Username' and Password = '$Password'";
+                        $result = mysqli_query($conn, $query)
+                                    or die ("Error in query" . mysqli_error($conn));;
+                           if(mysqli_num_rows($result) == 0){
+                                   $row = mysqli_fetch_row($result);
+                                   echo "<p style='color:red; margin-left:5%;'>$row[1] Email or Password does not exist</p>";
+                               }
+                            else{
+                                $row = mysqli_fetch_row($result);
+                                $email = $row[1];
+                                $_SESSION['Email'] = $email;
+                                $name = $row[3];
+                                $_SESSION['name'] = $name;
+                                $AccountID =  $row[0];  
+                                $_SESSION['AccountID'] = $AccountID;
+                                 echo "<p style='margin-left:5%;'>Name is $_SESSION[Email] <br/></p>";
+                                echo "<p style='margin-left:5%;'>Name is $_SESSION[name] <br/></p>";
+                                echo "<p style='margin-left:5%;'>Your AccountID is $_SESSION[AccountID]</p>";
+                            }
+        }
+        
+                    ?>
+                    
+                    <button type="submit" class="btn btn-primary" style="margin-left:5%; margin-right:5%;" name="Submit" href="inde.php">Submit</button>
+
+                </form>
+                <br/>
+            </div>
+            
+            
+            <div id="SignUp">
+            <h5   style="font-weight: bold; text-align: center;">SIGN UP USING YOUR EMAIL ADRESS</h5>
+                <br/>
+            <div class="container">
+            <form method="POST" action="Data.php">
+              <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control"  aria-describedby="emailHelp" name="Email">
+                <small id="emailHelp" class="form-text text-muted">We'll send your order confirmation here.</small>
+              </div>
+               <div class="form-group">
+                <label for="exampleInputPassword1">First Name</label>
+                <input type="text" class="form-control"  name="FirstName">
+               </div>
+                <div class="form-group">
+                <label>Second Name</label>
+                <input type="text" class="form-control" name="Surname">
+              </div>
+              <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="Password">
+              </div>
+              
+                <br/>
+                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+             </form>
+                <br/>
+            </div>
+        </div>
+            </div>
+        
+            <div class="col-lg-4 nopadding"></div>
+        </div>
+        <?php
+            
+           if(isset($_POST['submit'])){
+            $name = $_POST['FirstName'];
+            $username = $_POST['Email'];
+            $numbers = $_POST['Surname'];
+            $comments = $_POST['Password'];
+            $emailTo = 'claude.bugeja.b42219@mcast.edu.mt'; #later
+            $Pass = "Mcast11001";
+            $conn = mysqli_connect('localhost', 'root', '', 'project') 
+                    or die('Cannot connect to db');
+            $query = "select Email from user where Email = '$username'";
+            $result = mysqli_query($conn, $query)
+                        or die ("Error in query" . mysqli_error($conn));;
+               if(mysqli_num_rows($result) > 0){
+                       $row = mysqli_fetch_row($result);
+                       echo "$row[0] Email is taken";
+                   }
+               else{
+                    
+                    $query = "INSERT INTO user (Email, Password, FirstName, Surname) VALUES ('$username', '$comments', '$name', '$numbers')";
+                    $result =mysqli_query($conn, $query)
+                                or die ("Error in query" . mysqli_error($conn));
+
+                    require($_SERVER['DOCUMENT_ROOT'].'/phpmailer/PHPMailerAutoload.php'); #Superglobal
+
+                    $mail = new PHPMailer();
+                    $mail->isSMTP();
+                    $mail->isHtml(true);
+                    $mail->Debugoutput = 'html';
+                    $mail->Host = "smtp.office365.com";
+                    #$mail->SMTPDebug = 2; #include client and server messages
+                    $mail->Port = 587;
+                    #$mail->Port = 465; #change to ssl SMTPSecure
+                    $mail->SMTPSecure = 'tls';
+                    $mail->SMTPAuth = true;
+                    $mail->Username = $emailTo;
+                    $mail->Password = $Pass;
+
+                    $mail->From = $emailTo;
+                    $mail->AddAddress($emailTo); #later
+                    $mail->Subject = 'test';
+                    $mail->Body = "Email : ".$username."<br/> First Name : ".$name."<br/> Surname : ".$numbers."<br/> Password : ".$comments;
+
+                    #$mail->WordWrap = 50;
+
+
+                        if(!$mail->Send()) {
+                        echo "<script type='text/javascript'>alert('Account unsuccessfully created');</script>";
+
+                    } else {
+                        echo "<script type='text/javascript'>alert('Account successfully created');</script>";
+                    }
+               }
+               
+        }
+        ?>
+        
+    
+        <script>
+            document.getElementById("LogIn").style.display = "none";
+            document.getElementById("SignUp").style.display = "block";
+            function myFunction() {
+                var x = document.getElementById("LogIn");
+                var y = document.getElementById("SignUp");
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                    y.style.display = "none";
+                } else {
+                    x.style.display = "none";
+                    y.style.display = "block";
+                }
+            }
+        </script>
+    
+    </body>
+</html>
